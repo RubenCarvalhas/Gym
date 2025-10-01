7236942886781dcd2be4a319dddf7fd99542832f
unknown <carvalhasruben@gmail.com>
2025-10-01 02:28:14 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const scrollers = document.querySelectorAll(".scroller");
+
+if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    scrollers.forEach(scroller => {
+        scroller.setAttribute("data-animated", true);
+
+        const scrollerInner = scroller.querySelector('.scroller__inner');
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach(item => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute('aria-hidden', true);
+            scrollerInner.appendChild(duplicatedItem);
+        });
+    });
+}
+
+const hamburger = document.querySelector('.hamburger');
+const leftNav = document.querySelector('.left-side-nav');
+
+hamburger.addEventListener('click', () => {
+    leftNav.classList.toggle('active');
+});
+
+
